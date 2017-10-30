62a3320c0961f8e2be3334e676abea9a57dfb75f
cris1314 <cristopherjrivasc@gmail.com>
2017-10-30 14:40:39 -0400

    Frames Added

---
+++ menu.js
@@ -0,0 +1,17 @@
+var principalFramce = document.getElementById("principal-frame");
+var home = "inicio.html";
+var novelas = "novelas.html";
+var miembros = "miembros.html";
+var games = "videojuegos.html";
+var historia = "historia.html";
+var mercenazgo = "mercenazgo.html";
+
+function inicio() {
+    
+                principalFramce.src = novelas;
+
+            
+            
+    }
+}
+
